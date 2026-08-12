3c74cf5d49549e2d0bb0f0e32539186c0ac87d30
clofour <208706398+clofour@users.noreply.github.com>
2026-08-12 13:37:52 +0300

    implement welcoming functionality

---
+++ main.ts
@@ -0,0 +1,22 @@
+import { App, ExpressReceiver } from "@slack/bolt";
+
+const receiver = new ExpressReceiver({
+    signingSecret: process.env.SLACK_SIGNING_SECRET!,
+    processBeforeResponse: true,
+})
+
+const app = new App({
+    token: process.env.SLACK_BOT_TOKEN!,
+    receiver: receiver,
+})
+
+app.event("member_joined_channel", async ({ event, client }) => {
+    const channel = event.channel;
+
+    if (event.channel == process.env.CHANNEL_ID!) {
+        await client.chat.postMessage({
+            channel: channel,
+            text: "Hi",
+        })
+    }
+})
